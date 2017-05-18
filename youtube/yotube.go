e38e72f9d815c323337a94a7a226fd89216d9c28
Batbold Dashzeveg <dbatbold@gmail.com>
2017-05-18 00:31:40 +0000

    Added new resume, rename, itag switches

--- youtube/yotube.go
+++ youtube/yotube.go
@@ -20,6 +20,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -27,10 +29,21 @@ import (
 
 // _________________________________________________________________
 
-// Youtube video meta source url
-const URL_META = "http://www.youtube.com/get_video_info?&video_id="
+const (
+	// Youtube video meta source url
+	URL_META = "http://www.youtube.com/get_video_info?&video_id="
+)
 
-var FORMATS []string = []string{"3gp", "mp4", "flv", "webm", "avi"}
+const (
+	KB float64 = 1 << (10 * (iota + 1))
+	MB
+	GB
+)
+
+var (
+	// Video formats
+	FORMATS = []string{"3gp", "mp4", "flv", "webm", "avi"}
+)
 
 // holds a video's information
 type Video struct {
@@ -38,6 +51,7 @@ type Video struct {
 	Avg_rating                                 float32
 	View_count, Length_seconds                 int
 	Formats                                    []Format
+	Filename                                   string
 }
 
 type Format struct {
@@ -63,72 +77,114 @@ func Get(video_id string) (Video, error) {
 	return meta, nil
 }
 
-func (video *Video) Download(index int, filename string) error {
-	out, err := os.Create(filename)
-	if err != nil {
-		return fmt.Errorf("Unable to write to file %q: %s", filename, err)
+func (video *Video) Download(index int, filename string, resume, rename bool) error {
+	var (
+		out    *os.File
+		err    error
+		offset int64
+	)
+
+	if resume {
+		// Resume download from last known offset
+		flags := os.O_WRONLY | os.O_CREATE
+		out, err = os.OpenFile(filename, flags, 0644)
+		if err != nil {
+			return fmt.Errorf("Unable to open file %q: %s", filename, err)
+		}
+		offset, err = out.Seek(0, os.SEEK_END)
+		if err != nil {
+			return fmt.Errorf("Unable to seek file %q: %s", filename, err)
+		}
+		fmt.Printf("Resuming from offset %d (%s)\n", offset, abbr(offset))
+
+	} else {
+		// Start new download
+		flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+		out, err = os.OpenFile(filename, flags, 0644)
+		if err != nil {
+			return fmt.Errorf("Unable to write to file %q: %s", filename, err)
+		}
 	}
 	defer out.Close()
 
 	url := video.Formats[index].Url
+	video.Filename = filename
 
 	// Check if server accepts range request
 	if resp, err := http.Head(url); err != nil {
 		return fmt.Errorf("Head request failed: %s", err)
+
 	} else if resp.Header.Get("Accept-Ranges") == "bytes" {
+		// Download in chunks
 		var length int64
 		if size := resp.Header.Get("Content-Length"); len(size) == 0 {
 			return errors.New("Content-Length header is missing")
 		} else if length, err = strconv.ParseInt(size, 10, 64); err != nil {
 			return fmt.Errorf("Invalid Content-Length: %s", err)
 		}
-		return video.DownloadChunks(out, length, url)
+		if length == offset {
+			fmt.Println("Video file is already dowloaded.")
+			return nil
+		}
+		if err := video.DownloadChunks(out, length, url, offset); err != nil {
+			return err
+		}
+
+	} else {
+		// No range support, download without progress print
+		resp, err := http.Get(url)
+		if err != nil {
+			return fmt.Errorf("Request failed: %s", err)
+		}
+		defer resp.Body.Close()
+
+		if _, err := io.Copy(out, resp.Body); err != nil {
+			return err
+		}
 	}
 
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("Request failed: %s", err)
+	if rename {
+		// Rename output file using video title
+		wspace := regexp.MustCompile(`\W+`)
+		fname := strings.Split(filename, ".")[0]
+		ext := filepath.Ext(filename)
+		title := wspace.ReplaceAllString(video.Title, "-")
+		if len(title) > 64 {
+			title = title[:64]
+		}
+		title = strings.ToLower(title)
+		video.Filename = fmt.Sprintf("%s-%s%s", fname, title, ext)
+		if err := os.Rename(filename, video.Filename); err != nil {
+			fmt.Println("Failed to rename output file:", err)
+		}
 	}
-	defer resp.Body.Close()
-
-	if err != nil {
-		return errors.New("Unable to download video content from Yotutube")
-	}
-
-	io.Copy(out, resp.Body)
 
 	return nil
 }
 
+func abbr(byteSize int64) string {
+	size := float64(byteSize)
+	switch {
+	case size > GB:
+		return fmt.Sprintf("%.1fGB", size/GB)
+	case size > MB:
+		return fmt.Sprintf("%.1fMB", size/MB)
+	case size > KB:
+		return fmt.Sprintf("%.1fKB", size/KB)
+	}
+	return fmt.Sprintf("%d", byteSize)
+}
+
 // Downloads video content in chunks and prints progress
-func (video *Video) DownloadChunks(out *os.File, length int64, url string) error {
+func (video *Video) DownloadChunks(out *os.File, length int64,
+	url string, offset int64) error {
 
 	var (
-		offset int64
-		chunk  int64 = 10 * 1 << 20 // 10MB
+		chunk  int64 = 1 << 20 // 1MB
 		ticker       = time.NewTicker(time.Second)
 	)
 
 	defer ticker.Stop()
-
-	const (
-		KB float64 = 1 << (10 * (iota + 1))
-		MB
-		GB
-	)
-
-	abbr := func(byteSize int64) string {
-		size := float64(byteSize)
-		switch {
-		case size > GB:
-			return fmt.Sprintf("%0.1fGB", size/GB)
-		case size > MB:
-			return fmt.Sprintf("%0.1fMB", size/MB)
-		case size > KB:
-			return fmt.Sprintf("%0.1fKB", size/KB)
-		}
-		return fmt.Sprintf("%.0f", size)
-	}
 
 	printProgress := func() {
 		start := time.Now()
@@ -194,7 +250,7 @@ func (video *Video) DownloadChunks(out *os.File, length int64, url string) error
 			break
 		}
 
-		if offset == length {
+		if offset >= length {
 			time.Sleep(time.Second)
 			fmt.Println()
 			break
@@ -208,14 +264,24 @@ func (video *Video) DownloadChunks(out *os.File, length int64, url string) error
 }
 
 // figure out the file extension from a codec string
-func (video *Video) GetExtension(index int) string {
-	for i := 0; i < len(FORMATS); i++ {
-		if strings.Contains(video.Formats[index].Video_type, FORMATS[i]) {
-			return FORMATS[i]
+func (v *Video) GetExtension(index int) string {
+	for _, format := range FORMATS {
+		if strings.Contains(v.Formats[index].Video_type, format) {
+			return format
 		}
 	}
 
 	return "avi"
+}
+
+// Returns video format index by Itag number, or -1 if unknown
+func (v *Video) IndexByItag(itag int) int {
+	for i, format := range v.Formats {
+		if format.Itag == itag {
+			return i
+		}
+	}
+	return -1
 }
 
 // _________________________________________________________________
